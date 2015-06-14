bf81a24cefd666d3ba99e7bfa406ea76add38aa2
fmal <crimhead@gmail.com>
2015-06-15 00:09:48 +0200

    Tweak tests

--- test/main.js
+++ test/main.js
@@ -1,7 +1,6 @@
 'use strict';
 
-var gulp = require('gulp'),
-    path = require('path'),
+var path = require('path'),
     gutil = require('gulp-util'),
     fs = require('fs'),
     test = require('tape'),
@@ -26,10 +25,14 @@ function getExpected (filePath) {
 function compare (stream, fixtureName, expectedName, t) {
     stream.on('data', function (newFile) {
         t.equal(String(getExpected(expectedName).contents), String(newFile.contents));
+    });
+
+    stream.on('end', function () {
         t.end();
     });
 
     stream.write(getFixture(fixtureName));
+    stream.end();
 }
 
 test('inlines <script> tag', function (t) {
@@ -40,8 +43,20 @@ test('inlines <link> tag', function (t) {
     compare(inlinesource(), 'link.html', 'inlined-link.html', t);
 });
 
+test('inlines <img> tag with SVG source', function (t) {
+    compare(inlinesource(), 'image-svg.html', 'inlined-image-svg.html', t);
+});
+
+test('inlines <img> tag with PNG source', function (t) {
+    compare(inlinesource(), 'image-png.html', 'inlined-image-png.html', t);
+});
+
 test('works with type and media attributes', function (t) {
     compare(inlinesource(), 'with-attributes.html', 'inlined-with-attributes.html', t);
+});
+
+test('works with relative paths', function (t) {
+    compare(inlinesource(), 'script-relative.html', 'inlined-script.html', t);
 });
 
 test('inlines assets without minification', function (t) {
@@ -51,4 +66,3 @@ test('inlines assets without minification', function (t) {
 
     compare(stream, 'nominify.html', 'inlined-nominify.html', t);
 });
-
